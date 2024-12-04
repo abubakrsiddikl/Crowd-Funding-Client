217e90e4fcdd46f4520850274c0072cf51ab7087
limon <limonsk026@gmail.com>
2024-12-04 10:52:17 +0600

    router setup

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 
 const MainLayout = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
         </div>
     );
 };
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,42 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../components/MainLayout/MainLayout";
+import AllCampaign from "../components/AllCampaign/AllCampaign";
+import AddNewCampaign from "../components/AddNewCampaign/AddNewCampaign";
+import MyCampaign from "../components/MyCampaign/MyCampaign";
+import MyDonation from "../components/MyDonation/MyDonation";
+import Login from "../components/Auth/Login";
+import Register from "../components/Auth/Register";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
     },
+    {
+      path: "/allCampaign",
+      element: <AllCampaign></AllCampaign>
+    },
+    {
+      path: "/addNewCampaign",
+      element: <AddNewCampaign></AddNewCampaign>
+    },
+    {
+      path: "/myCampaign",
+      element: <MyCampaign></MyCampaign>
+    },
+    {
+      path: "/myDonation",
+      element: <MyDonation></MyDonation>
+    },
+    {
+      path: "/login",
+      element: <Login></Login>
+    },
+    {
+      path: "/register",
+      element: <Register></Register>
+    }
   ]);
   
 
